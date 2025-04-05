17ed81b262b92196b167c7fd66eac296302d27c7
icepanda473 <bingcuanxiongmao666@outlook.com>
2025-04-05 21:40:49 +0800

    Site updated: 2025-04-05 21:40:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/04/hello-world/","2025/04/04/我的第一篇博客/"];function toRandomPost(){
+var posts=["2025/04/04/hello-world/","2025/04/05/资源整理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
